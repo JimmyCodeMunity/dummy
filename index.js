35d31dce9f5c4eae8d97dd49a188afbb028118c9
Jimmy <96749667+JimmyCodeMunity@users.noreply.github.com>
2023-12-15 12:17:57 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const Product = require('./models/ProductModel')
+const productRoute = require('./routes/ProductRoute')
 
 
 
