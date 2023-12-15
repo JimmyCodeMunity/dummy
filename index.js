5ffc6319f3f2ed521f1011412adafcf466afddeb
Jimmy <96749667+JimmyCodeMunity@users.noreply.github.com>
2023-12-15 12:05:00 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,47 +1,47 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const bodyParser = require('body-parser');
-
-
-require('dotenv').config();
-
+const Product = require('./models/ProductModel');
+const User = require('./models/UserModel');
+const productRoute = require('./routes/ProductRoute');
+const userRoute = require('./routes/UserRoute');
+const shopRoute = require('./routes/ShopRoutes');
 
 const app = express();
 
-const cors = require('cors');
-
-
-app.use(cors())
-
-
-//allow json requests to be sent to the server
-app.use(express.json())
-
-//allow url encoded for from input
-app.use(express.urlencoded({extended:false}))
-
-
-if(process.env.NODE_ENV !== 'PRODUCTION'){
-    require("dotenv").config({
-      path:"./.env"
-    })
-  }
-
-
-  const port = process.env.PORT;
-
-app.listen(port,(req,res) => {
-    console.log(`Server running on port ${port}`)
-})
-
+// Middleware
+app.use(cors());
+app.use(express.json()); // You can use express.json() instead of body-parser for JSON parsing
+app.use(express.urlencoded({ extended: false }));
 
 // Connect to MongoDB
-mongoose.connect("mongodb+srv://Collo:Collo77@cluster0.bo6bwv7.mongodb.net/test?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.error('Failed to connect to MongoDB', err));
-//strict query
-mongoose.set('strictQuery', true);
+const connectToMongoDB = async () => {
+  try {
+    await mongoose.connect('mongodb+srv://Collo:Collo77@cluster0.bo6bwv7.mongodb.net/test?retryWrites=true&w=majority', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Connected to MongoDB');
+  } catch (error) {
+    console.error('Failed to connect to MongoDB', error);
+  }
+};
 
-app.get('/',(req,res)=>{
-    res.send('Server started')
-})
+// Call the connectToMongoDB function to establish the MongoDB connection
+connectToMongoDB();
+
+// Routes
+app.use('/api/product', productRoute);
+app.use('/api/user', userRoute);
+app.use('/api/shop', shopRoute);
+
+app.get('/', (req, res) => {
+  res.send('Opasso server initiated');
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
